cd234fce25b81f35ceedb22fb57c28c041624245
shaileshwebdev <138277595+shaileshwebdev@users.noreply.github.com>
2025-06-01 01:49:26 +0530

    Update api.jsx

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://employeemanagementsystem-wnew.onrender.com';
 
 export const GetAllEmployees = async (search = '', page = 1, limit = 5) => {
     const url =
